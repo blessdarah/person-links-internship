8e84ef4ea31f6bd6558914cfb797720503b7ad11
user <blessdarahuba@gmail.com>
2025-04-29 09:25:35 +0100

    Updated connection class to be db agnostic
    Can work with mysql and pgsql by pulling from .env file

--- index.php
+++ index.php
@@ -1,161 +1,159 @@
 <?php
-require __DIR__ . "./vendor/autoload.php";
+require __DIR__.'/vendor/autoload.php';
 
+use Dotenv\Dotenv;
 use PersonLinks\Internship\Connection;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-use PersonLinks\Internship\Env;
 
-
-Env::load(__DIR__ . "/.env");
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 
 $db = Connection::getInstance();
 
-if (isset($_POST["register"])) {
-	$Fullname = $_POST['name'];
-	$Email = $_POST['email'];
-	$Phone = $_POST['phone'];
-	$School = $_POST['school'];
-	$Speciality = $_POST['speciality'];
-	$Referral = $_POST['referral'];
-	$Comments = $_POST['comments'];
-	$Applicant_ID = strtoupper(substr(
-		$Fullname,
-		0,
-		3
-	) . rand(100, 999));
+if (isset($_POST['register'])) {
+    $Fullname = $_POST['name'];
+    $Email = $_POST['email'];
+    $Phone = $_POST['phone'];
+    $School = $_POST['school'];
+    $Speciality = $_POST['speciality'];
+    $Referral = $_POST['referral'];
+    $Comments = $_POST['comments'];
+    $Applicant_ID = strtoupper(substr(
+        $Fullname,
+        0,
+        3
+    ).rand(100, 999));
 
-	$to = "$Email";
-	$subject = "Internship Pre-application Successful!";
-	$headers = "From: no-reply@personlinks.org\n\n";
-	$message = "Your Applicant ID is: " . $Applicant_ID .
-		". Take note of it because you will need it during payment" .
-		"\nYou have successfully submitted your application for internship" .
-		"To fully secure your position, proceed and make your payments as slated below." .
-		"\n Make your payment either by MTN mobile money or by Orange money." .
-		"Take a screenshot and return to the home page to enter your payment details accordingly." .
-		"USE YOUR Applicant ID as your payment reference.\n\n One Month (XAF 20,000) \t Two Months (XAF 40,000)\nMTN - Momo (678686249) \t Orange - OM (659496501)\nAccount name: TANGUH MUNJAM";
+    $to = "$Email";
+    $subject = 'Internship Pre-application Successful!';
+    $headers = "From: no-reply@personlinks.org\n\n";
+    $message = 'Your Applicant ID is: '.$Applicant_ID.
+        '. Take note of it because you will need it during payment'.
+        "\nYou have successfully submitted your application for internship".
+        'To fully secure your position, proceed and make your payments as slated below.'.
+        "\n Make your payment either by MTN mobile money or by Orange money.".
+        'Take a screenshot and return to the home page to enter your payment details accordingly.'.
+        "USE YOUR Applicant ID as your payment reference.\n\n One Month (XAF 20,000) \t Two Months (XAF 40,000)\nMTN - Momo (678686249) \t Orange - OM (659496501)\nAccount name: TANGUH MUNJAM";
 
-	$sql = "INSERT INTO register (applicant_id, fullname, email, phone, school, speciality, referral, comments) VALUES 
-		(:appId, :fullname, :email, :phone, :school, :speciality, :referral, :comments)";
-	$query = $db->prepare($sql);
-	$result = $query->execute([
-		"appId" => $Applicant_ID,
-		"fullname" => $Fullname,
-		"email" => $Email,
-		"phone" => $Phone,
-		"school" => $School,
-		"speciality" => $Speciality,
-		"referral" => $Referral,
-		"comments" => $Comments
-	]);
+    $sql = 'INSERT INTO register (applicant_id, fullname, email, phone, school, speciality, referral, comments) VALUES 
+		(:appId, :fullname, :email, :phone, :school, :speciality, :referral, :comments)';
+    $query = $db->prepare($sql);
+    $result = $query->execute([
+        'appId' => $Applicant_ID,
+        'fullname' => $Fullname,
+        'email' => $Email,
+        'phone' => $Phone,
+        'school' => $School,
+        'speciality' => $Speciality,
+        'referral' => $Referral,
+        'comments' => $Comments,
+    ]);
 
-	if ($result) {
+    if ($result) {
 
-		$statusMsg = "Application Successful!";
-		$mail = new PHPMailer();
+        $statusMsg = 'Application Successful!';
+        $mail = new PHPMailer;
 
-		$mail->SMTPDebug = SMTP::DEBUG_SERVER;
-		$mail->isSMTP();
-		$mail->Host = getenv('MAIL_HOST');
-		$mail->SMTPAuth = true;
-		$mail->Username = getenv('MAIL_USER');
-		$mail->Password = getenv('MAIL_PASSWORD');
-		$mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
-		$mail->Port = getenv('MAIL_PORT');
+        $mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = getenv('MAIL_HOST');
+        $mail->SMTPAuth = true;
+        $mail->Username = getenv('MAIL_USER');
+        $mail->Password = getenv('MAIL_PASSWORD');
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = getenv('MAIL_PORT');
 
-		//Recipients
-		$mail->setFrom(
-			getenv('MAIL_DEFAULT_FROM'),
-			getenv('APP_NAME')
-		);
+        // Recipients
+        $mail->setFrom(
+            getenv('MAIL_DEFAULT_FROM'),
+            getenv('APP_NAME')
+        );
 
-		$mail->addAddress($to, $Fullname);
+        $mail->addAddress($to, $Fullname);
 
-		//Content
-		$mail->isHTML(true);
-		$mail->Subject = 'Sample subject';
-		$mail->Body = 'This is the HTML message body <b>in bold!</b>';
-		$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+        // Content
+        $mail->isHTML(true);
+        $mail->Subject = 'Sample subject';
+        $mail->Body = 'This is the HTML message body <b>in bold!</b>';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
-		$mail->send();
+        $mail->send();
 
-		session_start();
-		$_SESSION['ApplicantID'] = $Applicant_ID;
-		$_SESSION['Fullname'] = $Fullname;
-		header('Location: src/submission.php');
-	} else {
-		$statusMsg = "Connection Failed!";
-	}
-	echo $statusMsg;
+        session_start();
+        $_SESSION['ApplicantID'] = $Applicant_ID;
+        $_SESSION['Fullname'] = $Fullname;
+        header('Location: src/submission.php');
+    } else {
+        $statusMsg = 'Connection Failed!';
+    }
+    echo $statusMsg;
 }
 
+if (isset($_POST['upload'])) {
+    $ApplicantID = $_POST['applicant_id'];
+    $Phone = $_POST['phone'];
+    $amount = $_POST['amount'];
+    $TransactionID = $_POST['transaction_id'];
+    $Date = date('Y-m-d H:i:s');
 
-if (isset($_POST["upload"])) {
-	$ApplicantID = $_POST['applicant_id'];
-	$Phone = $_POST['phone'];
-	$amount = $_POST['amount'];
-	$TransactionID = $_POST['transaction_id'];
-	$Date = date('Y-m-d H:i:s');
+    $targetDir = 'src/uploads/';
+    $fileName = $ApplicantID.basename($_FILES['file']['name']);
+    $targetFilePath = $targetDir.$fileName;
+    $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
 
-	$targetDir = "src/uploads/";
-	$fileName = $ApplicantID . basename($_FILES["file"]["name"]);
-	$targetFilePath = $targetDir . $fileName;
-	$fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION);
+    $sql = 'SELECT * FROM register WHERE applicant_id = :id';
+    $query = $db->prepare($sql);
+    $query->execute([
+        'id' => $ApplicantID,
+    ]);
 
-	$sql = "SELECT * FROM register WHERE applicant_id = :id";
-	$query = $db->prepare($sql);
-	$query->execute([
-		'id' => $ApplicantID,
-	]);
+    $row = $query->fetch();
+    // if not found, create new record
+    if (! $row) {
+        $statusMsg = 'You need to register first';
+    } else {
 
-	$row = $query->fetch();
-	//if not found, create new record
-	if (!$row) {
-		$statusMsg = "You need to register first";
-	} else {
+        $Amounts = $row['amount_paid'] + $amount;
 
-		$Amounts = $row['amount_paid'] + $amount;
+        if (! empty($_FILES['file']['tmp_name'])) {
+            // Allow certain file formats
+            $allowTypes = ['jpg', 'png', 'jpeg', 'JPG', 'JPEG', 'PNG', ''];
 
-		if (!empty($_FILES["file"]["tmp_name"])) {
-			// Allow certain file formats
-			$allowTypes = array('jpg', 'png', 'jpeg', 'JPG', 'JPEG', 'PNG', '');
-
-			if (in_array($fileType, $allowTypes)) {
-				// Upload file to server
-				if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath)) {
-					// Insert image file name into database
-					$sql = "UPDATE register 
+            if (in_array($fileType, $allowTypes)) {
+                // Upload file to server
+                if (move_uploaded_file($_FILES['file']['tmp_name'], $targetFilePath)) {
+                    // Insert image file name into database
+                    $sql = 'UPDATE register 
 						SET amount_paid = :amounts, 
 						trans_id = :transId, 
 						screenshot = :screenshotPath, 
 						uploaded_on = :uploadedOn 
-						WHERE applicant_id = :appId";
-					
-					$query = $db->prepare($sql);
-					$result = $query->execute([
-						"transId" => $TransactionID,
-						"screenshot" => $targetFilePath,
-						"uploadedOn" => $Date
-					]);
-					header('Location: src/submission2.php');
+						WHERE applicant_id = :appId';
 
-					if ($result) {
-						$statusMsg = "Application Successful!";
-					} else {
-						$statusMsg = "File upload failed, please try again.";
-					}
-				} else {
-					$statusMsg = "Sorry, there was an error uploading your file.";
-				}
-			} else {
-				$statusMsg = 'Sorry, only JPG, JPEG, PNG files are allowed to upload.';
-			}
-		}
-	}
+                    $query = $db->prepare($sql);
+                    $result = $query->execute([
+                        'transId' => $TransactionID,
+                        'screenshot' => $targetFilePath,
+                        'uploadedOn' => $Date,
+                    ]);
+                    header('Location: src/submission2.php');
 
+                    if ($result) {
+                        $statusMsg = 'Application Successful!';
+                    } else {
+                        $statusMsg = 'File upload failed, please try again.';
+                    }
+                } else {
+                    $statusMsg = 'Sorry, there was an error uploading your file.';
+                }
+            } else {
+                $statusMsg = 'Sorry, only JPG, JPEG, PNG files are allowed to upload.';
+            }
+        }
+    }
 
-	echo $statusMsg;
+    echo $statusMsg;
 }
 
 ?>
--- src/Connection.php
+++ src/Connection.php
@@ -5,45 +5,52 @@ namespace PersonLinks\Internship;
 use Exception;
 use PDO;
 
-
 class Connection
 {
-	
-	 /** @var PDO|null */
-	private static $instance = null;
-	public function __construct()
-	{
-	}
-	public function __wakeup()
-	{
-	}
-	public function __clone()
-	{
-	}
+    /** @var PDO|null */
+    private static $instance = null;
 
-	public static function getInstance(): PDO
-	{
-		if (self::$instance == null) {
-			$host = $_ENV["DB_HOST"];
-			$dbname = $_ENV['DB_NAME'];
-			$username = $_ENV['DB_USER'];
-			$password = $_ENV['DB_PASSWORD'];
-			$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
+    public function __construct() {}
 
-			try {
-				self::$instance = new PDO(
-					$dsn,
-					$username,
-					"", // TODO: Change to use env password value
-					[
-						PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-						PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-					]
-				);
-			} catch (Exception $e) {
-				throw new \PDOException($e->getMessage(), (int) $e->getCode());
-			}
-		}
-		return self::$instance;
-	}
+    public function __wakeup() {}
+
+    public function __clone() {}
+
+    public static function getInstance(): PDO
+    {
+        if (self::$instance == null) {
+            self::$instance = self::buildDsn($_ENV['DB_ENGINE'])();
+        }
+
+        return self::$instance;
+    }
+
+    protected static function buildDsn(string $dbEngine)
+    {
+        return match ($dbEngine) {
+            'mysql' => function () {
+                $dsn = "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8mb4";
+                try {
+                    $pdo = new PDO($dsn, $_ENV['DB_USER'], $_ENV['DB_PASSWORD']);
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    return $pdo;
+
+                } catch (Exception $e) {
+                    throw new \PDOException($e->getMessage(), (int) $e->getCode());
+                }
+            },
+            'pgsql' => function () {
+                $dsn = "pgsql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};user={$_ENV['DB_USER']};password={$_ENV['DB_PASSWORD']}";
+                try {
+                    $pdo = new PDO($dsn);
+                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                    return $pdo;
+                } catch (Exception $e) {
+                    throw new \PDOException($e->getMessage(), (int) $e->getCode());
+                }
+            }
+        };
+    }
 }
